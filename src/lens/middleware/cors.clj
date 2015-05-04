73a3cb95dfba697119c456543362862c838ffff5
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-04 15:53:06 +0200

    Improve CORS

--- src/lens/middleware/cors.clj
+++ src/lens/middleware/cors.clj
@@ -1,12 +1,18 @@
 (ns lens.middleware.cors)
 
 (defn assoc-header [response]
-  (assoc-in response [:headers "Access-Control-Allow-Origin"] "*"))
+  (-> (assoc-in response [:headers "Access-Control-Allow-Origin"] "*")
+      (assoc-in [:headers "Access-Control-Expose-Headers"] "ETag")))
 
 (defn wrap-cors
   "Adds an Access-Control-Allow-Origin header with the value * to responses."
   [handler]
   (fn [request]
-    (-> request
-        (handler)
-        (assoc-header))))
+    (if (= :options (:request-method request))
+      {:status 204
+       :headers {"Access-Control-Allow-Origin" "*"
+                 "Access-Control-Allow-Methods" "GET, POST, PUT"
+                 "Access-Control-Allow-Headers" "Authorization, Accept, If-Match"
+                 "Access-Control-Max-Age" "3600"}}
+      (-> (handler request)
+          (assoc-header)))))
