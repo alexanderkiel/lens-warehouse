def845cb3877cc7ba7a5a0011bc1000012b311d1
Alexander Kiel <alexanderkiel@gmx.net>
2015-07-10 13:07:30 +0200

    Allow All Known Data Types for Item Def

--- src/lens/handler/study.clj
+++ src/lens/handler/study.clj
@@ -90,7 +90,8 @@
     :name {:type s/Str}
     :desc {:type s/Str :optional true}}})
 
-(def item-def-data-type-schema (s/enum :text :integer))
+(def item-def-data-type-schema (s/enum :text :integer :float :date :time
+                                       :datetime :string :boolean))
 
 (defn render-create-item-def-form [path-for study]
   {:href (child-action-path :item-def :create path-for study)
