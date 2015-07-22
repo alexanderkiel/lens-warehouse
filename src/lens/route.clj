117d6669634c6e83993a6c5349b96be6088773f0
Alexander Kiel <alexanderkiel@gmx.net>
2015-07-22 18:56:17 +0200

    Optimize Routes for Performance
    The order of routes is important under a performance view because they
    are processed in order. As maps have no defined order, I use vectors of
    pairs instead. I also separated the :eid from path segments because
    pure string path segments are processed without compiling a regex first.
    For the :eid there is still a regex compiled.

--- src/lens/route.clj
+++ src/lens/route.clj
@@ -1,83 +1,89 @@
 (ns lens.route)
 
+(defn eid-handler [handler]
+  {[:eid] handler})
+
 (defn routes [context-path]
   (assert (re-matches #"/(?:.*[^/])?" context-path))
   [(if (= "/" context-path) "" context-path)
-   {(if (= "/" context-path) "/" "") :service-document-handler
+   [["/item-def/" (eid-handler :item-def-handler)]
 
-    ["/study/" :eid]
-    {"" :study-handler
-     "/find-study-event-def" :find-study-event-def-handler
-     "/find-form-def" :find-form-def-handler
-     "/find-item-group-def" :find-item-group-def-handler
-     "/find-item-def" :find-item-def-handler
+    ["/item-ref/" (eid-handler :item-ref-handler)]
 
-     "/study-event-defs"
-     {"" :create-study-event-def-handler
-      ["/page/" :page-num] :study-study-event-defs-handler}
+    ["/study/"
+     (eid-handler
+       [["/find-item-def" :find-item-def-handler]
+        ["/find-item-group-def" :find-item-group-def-handler]
+        ["/find-form-def" :find-form-def-handler]
+        ["/find-study-event-def" :find-study-event-def-handler]
 
-     "/form-defs"
-     {"" :create-form-def-handler
-      ["/page/" :page-num] :study-form-defs-handler}
+        ["" :study-handler]
 
-     "/item-group-defs"
-     {"" :create-item-group-def-handler
-      ["/page/" :page-num] :study-item-group-defs-handler}
+        ["/study-event-defs"
+         {"" :create-study-event-def-handler
+          ["/page/" :page-num] :study-study-event-defs-handler}]
 
-     "/item-defs"
-     {"" :create-item-def-handler
-      ["/page/" :page-num] :study-item-defs-handler}
+        ["/form-defs"
+         {"" :create-form-def-handler
+          ["/page/" :page-num] :study-form-defs-handler}]
 
-     "/subjects"
-     {"" :create-subject-handler}}
+        ["/item-group-defs"
+         {"" :create-item-group-def-handler
+          ["/page/" :page-num] :study-item-group-defs-handler}]
 
-    ["/study-event-def/" :eid]
-    {"" :study-event-def-handler
-     "/find-form-ref" :find-form-ref-handler
-     "/form-refs"
-     {"" :create-form-ref-handler
-      ["/page/" :page-num] :form-refs-handler}}
+        ["/item-defs"
+         {"" :create-item-def-handler
+          ["/page/" :page-num] :study-item-defs-handler}]
 
-    ["/form-ref/" :eid]
-    {"" :form-ref-handler}
+        ["/subjects"
+         {"" :create-subject-handler}]])]
 
-    ["/form-def/" :eid]
-    {"" :form-def-handler
-     "/find-item-group-ref" :find-item-group-ref-handler
-     "/item-group-refs"
-     {"" :create-item-group-ref-handler
-      ["/page/" :page-num] :item-group-refs-handler}}
+    ["/item-group-def/"
+     (eid-handler
+       [["/find-item-ref" :find-item-ref-handler]
+        ["" :item-group-def-handler]
+        ["/item-refs"
+         {"" :create-item-ref-handler
+          ["/page/" :page-num] :item-refs-handler}]])]
 
-    ["/item-group-ref/" :eid]
-    {"" :item-group-ref-handler}
+    ["/item-group-ref/" (eid-handler :item-group-ref-handler)]
 
-    ["/item-group-def/" :eid]
-    {"" :item-group-def-handler
-     "/find-item-ref" :find-item-ref-handler
-     "/item-refs"
-     {"" :create-item-ref-handler
-      ["/page/" :page-num] :item-refs-handler}}
+    ["/form-def/"
+     (eid-handler
+       {"" :form-def-handler
+        "/find-item-group-ref" :find-item-group-ref-handler
+        "/item-group-refs"
+        {"" :create-item-group-ref-handler
+         ["/page/" :page-num] :item-group-refs-handler}})]
 
-    ["/item-ref/" :eid]
-    {"" :item-ref-handler}
+    ["/form-ref/" (eid-handler :form-ref-handler)]
 
-    ["/item-def/" :eid]
-    {"" :item-def-handler}
+    ["/study-event-def/"
+     (eid-handler
+       {"" :study-event-def-handler
+        "/find-form-ref" :find-form-ref-handler
+        "/form-refs"
+        {"" :create-form-ref-handler
+         ["/page/" :page-num] :form-refs-handler}})]
 
-    "/studies"
-    {"" :create-study-handler
-     ["/page/" :page-num] :all-studies-handler}
+    ["/studies"
+     {"" :create-study-handler
+      ["/page/" :page-num] :all-studies-handler}]
 
-    ["/code-lists" :id] :code-list-handler
-    "/snapshots"
-    {"" :all-snapshots-handler
-     ["/" :id] :snapshot-handler
-     ["/" :id "/query"] :query-handler}
-    "/most-recent-snapshot" :most-recent-snapshot-handler
-    "/find-study" :find-study-handler
+    [["/code-lists" :id] :code-list-handler]
 
-    "/study-profile" :study-profile-handler
-    "/study-event-def-profile" :study-event-def-profile-handler
-    "/form-def-profile" :form-def-profile-handler
-    "/item-group-def-profile" :item-group-def-profile-handler
-    "/item-def-profile" :item-def-profile-handler}])
+    ["/snapshots"
+     {"" :all-snapshots-handler
+      ["/" :id] :snapshot-handler
+      ["/" :id "/query"] :query-handler}]
+
+    ["/most-recent-snapshot" :most-recent-snapshot-handler]
+    ["/find-study" :find-study-handler]
+
+    ["/study-profile" :study-profile-handler]
+    ["/study-event-def-profile" :study-event-def-profile-handler]
+    ["/form-def-profile" :form-def-profile-handler]
+    ["/item-group-def-profile" :item-group-def-profile-handler]
+    ["/item-def-profile" :item-def-profile-handler]
+
+    [(if (= "/" context-path) "/" "") :service-document-handler]]])
