fe421888a577612fd719569509c2e653e55f885b
Alexander Kiel <alexanderkiel@gmx.net>
2015-07-26 17:39:41 +0200

    Add X-Datomic-As-Of-T Header Handling

--- src/lens/middleware/datomic.clj
+++ src/lens/middleware/datomic.clj
@@ -1,5 +1,8 @@
 (ns lens.middleware.datomic
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [schema.core :as s]
+            [schema.coerce :as c]
+            [schema.utils :as su]))
 
 (defn- assoc-conn
   [request conn]
@@ -7,15 +10,36 @@
     (assoc request :conn conn)
     request))
 
-(defn- assoc-db
-  [request conn]
-  (assoc request :db (d/db conn)))
+(def ^:private int-coercer (c/coercer s/Int c/string-coercion-matcher))
+
+(defn- as-of-t [{:keys [headers]}]
+  (some-> (headers "x-datomic-as-of-t") (int-coercer)))
+
+(defn- db [conn as-of-t]
+  (if as-of-t
+    (d/as-of (d/db conn) as-of-t)
+    (d/db conn)))
+
+(defn- assoc-db [request conn as-of-t]
+  (assoc request :db (db conn as-of-t)))
 
 (defn- connect [uri]
   (try
     (d/connect uri)
     (catch Throwable e
       (str "Error connecting to " uri ": " (.getMessage e)))))
+
+(defn- connection-error-resp [conn]
+  {:status 503
+   :body
+   {:data
+    {:message conn}}})
+
+(defn as-of-t-error-resp [error]
+  {:status 400
+   :body
+   {:data
+    {:message (str "Invalid X-Datomic-As-Of-T value: " (.-value error))}}})
 
 (defn wrap-connection
   "Middleware which adds a connection and for GET requests a database to the
@@ -27,9 +51,11 @@
   (fn [request]
     (let [conn (connect uri)]
       (if (string? conn)
-        {:status 503
-         :body
-         {:data
-          {:message conn}}}
-        (let [request (-> request (assoc-conn conn) (assoc-db conn))]
-          (handler request))))))
+        (connection-error-resp conn)
+        (let [as-of-t (as-of-t request)]
+          (if (su/error? as-of-t)
+            (as-of-t-error-resp (su/error-val as-of-t))
+            (-> request
+                (assoc-conn conn)
+                (assoc-db conn as-of-t)
+                (handler))))))))
