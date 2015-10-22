a9fb942b66ba6233bd4250c5f050a87a0ca5a259
Alexander Kiel <alexanderkiel@gmx.net>
2015-10-22 16:36:06 +0200

    Respect DATOMIC_EDITION in start.sh
    Same as in Procfile.

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-lein with-profile production,datomic-free trampoline run -p 80 -d ${DB_URI:-datomic:free://db:4334/lens-warehouse} -c ${CONTEXT_PATH:-/}
+lein with-profile production,datomic-${DATOMIC_EDITION:-free} trampoline run -p 80 -d ${DB_URI:-datomic:free://db:4334/lens-warehouse} -c ${CONTEXT_PATH:-/}
