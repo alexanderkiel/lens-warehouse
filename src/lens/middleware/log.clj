a7da17afb3159c0bf6cf73c42f4e1060d45213e2
Alexander Kiel <alexanderkiel@gmx.net>
2016-01-26 17:32:57 +0100

    Retain Bodies of Error Responses

--- src/lens/middleware/log.clj
+++ src/lens/middleware/log.clj
@@ -2,12 +2,15 @@
   (:require [lens.logging :refer [info]]
             [lens.util :as util]))
 
+(defn- filter-response [{:keys [status] :as resp}]
+  (select-keys resp (cond-> [:status :headers] (<= 400 status) (conj :body))))
+
 (defn wrap-log [handler]
   (fn [req]
     (let [start (System/nanoTime)
           resp (handler req)]
       (info {:request (select-keys req [:remote-addr :request-method
                                         :uri :query-string :headers])
-             :response (select-keys resp [:status :headers])
+             :response (filter-response resp)
              :took (util/duration start)})
       resp)))
