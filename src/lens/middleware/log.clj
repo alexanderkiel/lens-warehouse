5ddf3f8a59336c1c9b985f4150ef61486356fe40
Alexander Kiel <alexanderkiel@gmx.net>
2016-02-09 18:37:48 +0100

    Fix NPE in Logging Middleware

--- src/lens/middleware/log.clj
+++ src/lens/middleware/log.clj
@@ -1,9 +1,10 @@
 (ns lens.middleware.log
-  (:require [lens.logging :refer [warn error]]))
+  (:require [plumbing.core :refer [letk]]
+            [lens.logging :refer [warn error]]))
 
 (defn wrap-log-errors [handler]
   (fn [req]
-    (let [{:keys [status] :as resp} (handler req)]
+    (letk [[{status 200} :as resp] (handler req)]
       (cond
         (<= 500 status) (error {:type :server-error-response :response resp})
         (<= 400 status) (warn {:type :client-error-response :response resp}))
